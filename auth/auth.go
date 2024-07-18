0190813bf7407d50f563283f61e0b9495106f8cc
AndrewOdiit <andrewodiit2020@gmail.com>
2024-07-18 13:16:15 +0300

    [core] add auth package

---
+++ auth/auth.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+
+	"github.com/aodr3w/logger"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var log = logger.NewStdoutLogger()
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("Password Encryption Error %v", err.Error())
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+func IsValidPassword(hashedPassword []byte, password string) bool {
+	err := bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	return err == nil
+}
+
+func GenerateToken() (string, error) {
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/aodr3w/keiji-core
 
 go 1.22.1
+
+require (
+	github.com/aodr3w/logger v0.1.0 // indirect
+	golang.org/x/crypto v0.25.0
+)
