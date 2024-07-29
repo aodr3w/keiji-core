6f3f163607e74564775caf98976d9e9f16ddd130
AndrewOdiit <andrewodiit2020@gmail.com>
2024-07-29 10:05:21 +0300

    add imports to template

--- templates/tasks/schedule/main.go
+++ templates/tasks/schedule/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/aodr3w/keiji-tasks/core"
+
 func main() {
 	/*
 		    DEFINE FUNCTION SCHEDULE HERE
@@ -7,4 +9,5 @@ func main() {
 			 core.NewSchedule().Run().Every(10).Seconds().Build()
 			)
 	*/
+	core.NewSchedule().Run().Every(10).Seconds().Build()
 }
