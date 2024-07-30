edef14c0368df174f55bee709d0a4fafab40f237
AndrewOdiit <andrewodiit2020@gmail.com>
2024-07-30 20:53:09 +0300

    add errors package to core

---
+++ errors/errors.go
@@ -0,0 +1,26 @@
+package errors
+
+import "fmt"
+
+type PathNotFound struct {
+	Message string
+}
+
+func (e *PathNotFound) Error() string {
+	return e.Message
+}
+
+func Is(target error) bool {
+	_, ok := target.(*PathNotFound)
+	return ok
+}
+
+func NewPathNotFound(path string) *PathNotFound {
+	return &PathNotFound{
+		fmt.Sprintf("path %v not found", path),
+	}
+}
+
+var ErrPathNotFound = func(msg string) *PathNotFound {
+	return NewPathNotFound(msg)
+}
